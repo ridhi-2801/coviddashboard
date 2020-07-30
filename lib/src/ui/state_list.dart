3f5a760f9583097d0e1ca59a78b0788a911922e8
Ridhi <ridhi2801@gmail.com>
2020-07-30 18:04:56 +0530

    commit

--- lib/src/ui/state_list.dart
+++ lib/src/ui/state_list.dart
@@ -1,43 +1,70 @@
-import 'package:coviddashboard/src/models/jsonClass.dart';
-import 'package:flutter/material.dart';
-import 'package:coviddashboard/src/ui/models/jsonClass.dart';
 import 'dart:convert';
-Json json=Json();
-class StateList extends StatelessWidget{
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+class StateList extends StatefulWidget {
+  @override
+  _StateListState createState() => _StateListState();
+}
+
+class _StateListState extends State<StateList> {
+  List<String> state=["AN","AP","AR","AS"];
+  List<String> stateName=["Andaman","Andhra Pradesh","Arunachal Pradesh","Assam"];
+  Future future;
+  var coronaData;
+  Future getData() async {
+    http.Response response = await http.get(
+        'https://api.covid19india.org/v4/data.json?api_key=%E2%80%9C3ad5ab2eaf05830044bfabbd1690e1dfe3124de7%E2%80%9D');
+    if (response.statusCode == 200) {
+      String data = response.body;
+      coronaData = jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  @override
+  void initState() {
+    future = getData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-       appBar: AppBar(
-         centerTitle: true,
-         title: Text('States',style: TextStyle(
-           color: Colors.white,
-         ),),
-       ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: ListView(
-          children: <Widget>[
-            for(int i=0;i<36;i++)
-              Padding (
-                padding: const EdgeInsets.only(top: 28.0, left: 8, right: 8),
-                child: DetailCards (state: "Andaman",
-                  activeCases: json.getActiveCases (0).toString (),
-                  recoveredCases: json.getRecoveredCases (0).toString (),
-                  testedCases: json.getTestedCases (0).toString (),),
-              ),
-              Padding (
-                padding: const EdgeInsets.only(top: 28.0, left: 8, right: 8),
-                child: DetailCards (state: "Andhra Pradesh",
-                  activeCases: json.getActiveCases (1).toString (),
-                  recoveredCases: json.getRecoveredCases (1).toString (),
-                  testedCases: json.getTestedCases (1).toString (),),
-              ),
-          ],
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'States',
+          style: TextStyle(
+            color: Colors.white,
+          ),
         ),
       ),
+      body: FutureBuilder(
+          future: future,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            return Container(
+              width: double.infinity,
+              height: double.infinity,
+              child: ListView.builder(
+                  itemBuilder: (context, index){
+                    return Padding(
+                      padding:
+                      const EdgeInsets.only(top: 28.0, left: 8, right: 8),
+                      child: DetailCards(
+                        state: stateName[index],
+                        activeCases: coronaData[state[index]]['total']['confirmed'].toString(),
+                        recoveredCases:
+                        coronaData[state[index]]['total']['recovered'].toString(),
+                        testedCases: coronaData[state[index]]['total']['tested'].toString(),
+                      ),
+                    );
+                  }
+                  ),
+            );
+          }),
     );
   }
 }
@@ -47,49 +74,58 @@ class DetailCards extends StatelessWidget {
   final String activeCases;
   final String testedCases;
   final String recoveredCases;
-  DetailCards({this.state,this.activeCases,this.recoveredCases,this.testedCases});
+  DetailCards(
+      {this.state, this.activeCases, this.recoveredCases, this.testedCases});
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Container(
-      height: height/4,
+      height: height / 4,
       width: double.infinity,
       child: Card(
         color: Colors.white,
-       elevation: 18,
-       shape: RoundedRectangleBorder(
-         borderRadius: BorderRadius.circular(25),
-       ),
+        elevation: 18,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25),
+        ),
         child: Padding(
-          padding: const EdgeInsets.only(top:16.0,left: 26),
+          padding: const EdgeInsets.only(top: 16.0, left: 26),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              Text(state,style: TextStyle(
-              fontSize: 20
-              ),),
-             Padding(
-               padding: const EdgeInsets.only(top:26.0,left: 56),
-               child: Column(
-                 crossAxisAlignment: CrossAxisAlignment.start,
-                 mainAxisAlignment: MainAxisAlignment.center,
-                 children: <Widget>[
-                   Text("Active Cases: $activeCases",style: TextStyle(
-                       fontSize: 15
-                   ),),
-                   SizedBox(height: 10,),
-                   Text("Recovered Cases: $recoveredCases",style: TextStyle(
-                       fontSize: 15
-                   ),),
-                   SizedBox(height: 10,),
-                   Text("Tested Cases: $testedCases",style: TextStyle(
-                       fontSize: 15
-                   ),),
-                 ],
-               ),
-             )
+              Text(
+                state,
+                style: TextStyle(fontSize: 20),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 26.0, left: 56),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Active Cases: $activeCases",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Recovered Cases: $recoveredCases",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Tested Cases: $testedCases",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
